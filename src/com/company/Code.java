7da2f23d9ca1bbe8d46c79f07aa496f3a28360bf
gbarth1 <grace.m.barth@gmail.com>
2016-11-10 14:26:11 -0500

    Initial commit

---
+++ src/com/company/Code.java
@@ -0,0 +1,38 @@
+package com.company;
+
+/**
+ * Created by gb647 on 11/10/16.
+ */
+public class Code
+{
+    private String myCode;
+
+    String code="grace123";
+
+    public Code(String code)
+    {
+        myCode=code;
+        int code2=myCode.length();
+
+        int r=0;
+        for(int myCode:r )
+            code2.eplace()
+    }
+
+    public String getCode()
+    {
+       return myCode;
+    }
+
+    public void hide(int p1, int p2)
+    {
+
+    }
+
+    public void recover(int p1, int p2)
+    {
+
+    }
+
+
+}
